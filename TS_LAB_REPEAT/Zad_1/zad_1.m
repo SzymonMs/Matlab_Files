0b3558b0bccb3f1916cc794a575f57d6ee2e33bf
SzymonMs <szymon.murawski@student.put.poznan.pl>
2022-01-05 18:41:40 +0100

    Update zad_1.m

--- TS_LAB_REPEAT/Zad_1/zad_1.m
+++ TS_LAB_REPEAT/Zad_1/zad_1.m
@@ -24,7 +24,7 @@ Delta=D;
 bieguny_2=eig(Alpha);
 [L2,M2]=ss2tf(Alpha,Beta,Gamma,Delta);
 %step(L2,M2)
-[y,t,x]=step(A,B,C,D);
+[y_tustin,t,x_tustin]=step(A,B,C,D);
 [y2,t,x2]=step(Alpha,Beta,Gamma,Delta);
 %plot(t,x,t,y,t,x2,t,y2); %nie s¹ bo to inne zmienne stanu
 %% Podpunkt 3
@@ -40,17 +40,57 @@ sys=ss(Alpha, Beta, Gamma, Delta);
 y_stat=-10:0.1:10;
 M_stat=[];
 for i=1:length(y_stat)
-[M,t,x]=step(sys*y_stat(i));
+[M,t,x_tustin]=step(sys*y_stat(i));
 M_stat(i)=M(end);
 end
 %plot(y_stat,M_stat);
 %wzmocnienie_statyczne=2 jako wsp. kierunkowy char. statycznej i wyliczony
 %z transmitancji.
 %% Podpunkt 5
-[y,t,x]=step(sys);
+[y_tustin,t,x_tustin]=step(sys);
 %plot(x(:,1),x(:,2))
 %% Podpunkt 6
 Tp=0.01;
 tustin=c2d(sys,Tp,'Tustin');
+A_tustin=tustin.A;
+B_tustin=tustin.B;
+C_tustin=tustin.C;
+D_tustin=tustin.D;
+A_euler=eye(2)+Tp*Alpha;
+B_euler=Beta*Tp;
+C_euler=Gamma;
+D_euler=Delta;
+M=30/Tp;
+x_tustin=[];
+y_tustin=[];
+x_tustin(:,1)=[0 ;0];
+for k=1:M
+    x_tustin(:,k+1)=A_tustin*x_tustin(:,k)+B_tustin;
+    y_tustin(:,k)=C_tustin*x_tustin(:,k)+D_tustin;
+
+end
+% figure;
+% step(tustin)
+% figure;
+% plot(y_tustin);
+% figure;
+% plot(x_tustin);
+
+x_euler=[];
+y_euler=[];
+x_euler(:,1)=[0 ;0];
+for k=1:M
+    x_euler(:,k+1)=A_tustin*x_euler(:,k)+B_tustin;
+    y_euler(:,k)=C_tustin*x_euler(:,k)+D_tustin;
+
+end
+% figure;
+% plot(y_euler);
+% figure;
+% plot(x_euler);
+%% Podpunkt 7
+
+
+
 
 
