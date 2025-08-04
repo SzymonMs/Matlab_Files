cb53451a70fbdb7c80f3598b60ac0c784fb48049
SzymonMs <84814415+SzymonMs@users.noreply.github.com>
2025-08-04 19:28:01 +0200

    Added save data to csv files and clean up code

--- PQ_TEST/PQ_analayzer.m
+++ PQ_TEST/PQ_analayzer.m
@@ -48,7 +48,7 @@ for i = 1:3
    end 
 end
 %% Phase angles 200ms
-AnglesSamples = zeros(size(voltageSamples(:,1),2),6);
+agnles200msSamples = zeros(size(voltageSamples(:,1),2),6);
 for i = 1:3
     for j = 1:numberOfWindows200Cycle
         idxStart = (j-1)*samplesIn200ms+1;
@@ -60,38 +60,49 @@ for i = 1:3
         frequenceI = (0:size(tempI,1)-1) * (Ts*1000/size(tempI,1));
         [~, idx50U] = min(abs(frequenceU-nominalFrequency));
         [~, idx50I] = min(abs(frequenceI-nominalFrequency));
-        AnglesSamples(j,i) = rad2deg(angle(tempU(idx50U))-angle(firstPhaseFFT(idx50U)));
-        AnglesSamples(j,i+3) = rad2deg(angle(tempI(idx50I))-angle(firstPhaseFFT(idx50U)));
+        agnles200msSamples(j,i) = rad2deg(angle(tempU(idx50U))-angle(firstPhaseFFT(idx50U)));
+        agnles200msSamples(j,i+3) = rad2deg(angle(tempI(idx50I))-angle(firstPhaseFFT(idx50U)));
         clear tempU tempI idx50U idx50I frequenceI frequenceU;
     end
 end
 %% Phase-to-phase voltage 200ms
 % first - U12, second - U13, third - U23
-Phase2PhaseVoltageSamples = zeros(size(voltage200msSamples(:,1),1),3);
-Phase2PhaseVoltageSamples(:,1) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,2).^2 - 2*voltage200msSamples(:,1).*voltage200msSamples(:,2).*cosd(AnglesSamples(:,2)-AnglesSamples(:,1)));
-Phase2PhaseVoltageSamples(:,2) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,3).^2 - 2*voltage200msSamples(:,1).*voltage200msSamples(:,3).*cosd(AnglesSamples(:,3)-AnglesSamples(:,1)));
-Phase2PhaseVoltageSamples(:,3) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,3).^2 - 2*voltage200msSamples(:,2).*voltage200msSamples(:,3).*cosd(AnglesSamples(:,3)-AnglesSamples(:,2)));
+phase2PhaseVoltage200msSamples = zeros(size(voltage200msSamples(:,1),1),3);
+phase2PhaseVoltage200msSamples(:,1) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,2).^2 - 2*voltage200msSamples(:,1).*voltage200msSamples(:,2).*cosd(agnles200msSamples(:,2)-agnles200msSamples(:,1)));
+phase2PhaseVoltage200msSamples(:,2) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,3).^2 - 2*voltage200msSamples(:,1).*voltage200msSamples(:,3).*cosd(agnles200msSamples(:,3)-agnles200msSamples(:,1)));
+phase2PhaseVoltage200msSamples(:,3) = sqrt(voltage200msSamples(:,1).^2 + voltage200msSamples(:,3).^2 - 2*voltage200msSamples(:,2).*voltage200msSamples(:,3).*cosd(agnles200msSamples(:,3)-agnles200msSamples(:,2)));
 %% Avtive Power and Total Active Power 200ms
-ActivePowersSamples = zeros(size(voltage200msSamples(:,1),1),4);
+activePower200msSamples = zeros(size(voltage200msSamples(:,1),1),4);
 for i = 1:3
-   ActivePowersSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i).*cosd(AnglesSamples(:,i)-AnglesSamples(:,i+3));
+   activePower200msSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i).*cosd(agnles200msSamples(:,i)-agnles200msSamples(:,i+3));
 end
-ActivePowersSamples(:,4) = ActivePowersSamples(:,1)+ActivePowersSamples(:,2)+ActivePowersSamples(:,3);
+activePower200msSamples(:,4) = activePower200msSamples(:,1)+activePower200msSamples(:,2)+activePower200msSamples(:,3);
 %% Reactive Power and Total Reactive Power 200ms
-ReactivePowersSamples = zeros(size(voltage200msSamples(:,1),1),4);
+reactivePower200msSamples = zeros(size(voltage200msSamples(:,1),1),4);
 for i = 1:3
-   ReactivePowersSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i).*sind(AnglesSamples(:,i)-AnglesSamples(:,i+3));
+   reactivePower200msSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i).*sind(agnles200msSamples(:,i)-agnles200msSamples(:,i+3));
 end
-ReactivePowersSamples(:,4) = ReactivePowersSamples(:,1)+ReactivePowersSamples(:,2)+ReactivePowersSamples(:,3);
+reactivePower200msSamples(:,4) = reactivePower200msSamples(:,1)+reactivePower200msSamples(:,2)+reactivePower200msSamples(:,3);
 %% Apparent Power and Total Apparent Power 200ms
-ApparentPowersSamples = zeros(size(voltage200msSamples(:,1),1),4);
+apparentPower200msSamples = zeros(size(voltage200msSamples(:,1),1),4);
 for i = 1:3
-   ApparentPowersSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i);
+   apparentPower200msSamples(:,i) = voltage200msSamples(:,i).*current200msSamples(:,i);
 end
-ApparentPowersSamples(:,4) = ApparentPowersSamples(:,1)+ApparentPowersSamples(:,2)+ApparentPowersSamples(:,3);
+apparentPower200msSamples(:,4) = apparentPower200msSamples(:,1)+apparentPower200msSamples(:,2)+apparentPower200msSamples(:,3);
 
-
-
-
+%% Data Save to CSV Files
+dirName = "PQ_data";
+if ~exist(dirName, 'dir')
+    mkdir(dirName);
+end
+writetable(table(cat(2,voltageRMSSamples,currentRMSSamples)),strcat(dirName,"/RMS.csv"));
+writetable(table(cat(1,voltage200msSamples)),strcat(dirName,"/U_s.csv"));
+writetable(table(cat(1,current200msSamples)),strcat(dirName,"/I_s.csv"));
+writetable(table(cat(1,phase2PhaseVoltage200msSamples)),strcat(dirName,"/UU_s.csv"));
+writetable(table(cat(1,activePower200msSamples)),strcat(dirName,"/P_s.csv"));
+writetable(table(cat(1,reactivePower200msSamples)),strcat(dirName,"/Q_s.csv"));
+writetable(table(cat(1,apparentPower200msSamples)),strcat(dirName,"/S_s.csv"));
+writetable(table(cat(1,agnles200msSamples)),strcat(dirName,"/ANG_s.csv"));
+clear dirName
 %% Clean workspace
 clear i j idxEnd idxStart;
